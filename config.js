b3e0da2a7f64dc1130683640a175dff40eb6a146
Bill Vasilopoulos <billvasilopoulos@Bills-MBP-2.domain>
2019-12-14 22:06:48 -0800

    added project files

---
+++ config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteTitle: 'Portfolio Site', // <title>
+  manifestName: 'Grayscale',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Bill Vasilopoulos',
+  subHeading: 'Front End Web Developer',
+
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/bvasilop',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/billvasilopoulos/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/',
+    },
+    {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://facebook.com/billvasilopoulos',
+    },
+  ],
+  email: 'bvasilopgmail.com',
+  phone: '000-00000',
+  address: 'Seattle, Washington',
+};
